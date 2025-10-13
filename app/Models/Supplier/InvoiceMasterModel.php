9267794e2d959c0fbbb514fd90c2da2cde074369
Mahmud <machmudseo@gmail.com>
2025-10-14 02:43:52 +0700

    del id

--- app/Models/Supplier/InvoiceMasterModel.php
+++ app/Models/Supplier/InvoiceMasterModel.php
@@ -12,7 +12,7 @@ class InvoiceMasterModel extends Model
     protected $returnType       = 'object';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['id', 'nama', 'tipe'];
+    protected $allowedFields    = ['nama', 'tipe'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
